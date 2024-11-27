d05d9c8b283f5caba3a174d3f03502c293770359
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-11-27 19:25:04 +0530

    added user db

---
+++ src/app/sync-user/page.tsx
@@ -0,0 +1,34 @@
+import { db } from "@/server/db";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
+const SyncUser = async () => {
+  console.log("Syncing user");
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("No user");
+  }
+  const client = await clerkClient();
+  const user = await client.users.getUser(userId);
+  if (!user.emailAddresses[0]?.emailAddress) {
+    return notFound();
+  }
+  await db.user.upsert({
+    where: { emailAddress: user.emailAddresses[0]?.emailAddress ?? "" },
+    update: {
+      imageUrl: user.imageUrl,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+    create: {
+      id: userId,
+      emailAddress: user.emailAddresses[0]?.emailAddress ?? "",
+      imageUrl: user.imageUrl,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+  });
+  return redirect("/dashboard");
+};
+
+export default SyncUser;
